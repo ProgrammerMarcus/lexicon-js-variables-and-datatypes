13eb274ec1e15c5379004bfb5059c5b9e275efe0
Marcus <125326139+ProgrammerMarcus@users.noreply.github.com>
2024-01-10 13:57:34 +0100

    done

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ console.log(v3)
  */
 const name = "name"
 console.log(name)
-name = "noname"
+//name = "noname"
 console.log(name)
 // no, change const to let
 
@@ -49,17 +49,27 @@ console.log(name)
  * Discuss it with another person.
  */
 
+// toggling dark or light mode
+
 /**
  * 6. If we create a variable that we know is not going to
  * change its value during the duration of our program,
  * should we declare it using `let` or `const` and why?
  */
 
+// const, since it prevents accidental changes to the value
+// also throws errors if a change is attempted
+
 /**
  * 7. Create two numbers, save them in the variables
  * `number1` and `number2`. Add these two variabels together
  * and save the `result` in a new variable that you log to the console.
  */
+
+const number1 = 100
+const number2 = 200
+const result = number1 + number2
+console.log(result)
 
 /**
  * 8. Reuse the `number1` and `number2` variables and create three new variables:
@@ -69,11 +79,21 @@ console.log(name)
    - Log them all to the console.
  */
 
+const result1 = number1 - number2
+const result2 = number1 * number2
+const result3 = number1 / number2
+console.log(result1, result2, result3)
+
 /**
  * 9. Create a variable `a` and assign it a value.
  * Log it to the console. Now add 5 to `a` by reassigning the value of `a`
  * by using the existing `a` and the number 5. Log it to the console.
  */
+
+let a = 5
+console.log(a)
+a += 5
+console.log(a)
 
 /**
  * 10. What is the value of `a` in the console.log?
@@ -88,6 +108,19 @@ console.log(a);
 
 */
 
+a = 10;
+a = a - 4;
+a = 7;
+a = a + 2;
+
+console.log(a); // 9
+
 /**
  * 11. Explain each line of code in the previous question for a friend.
  */
+
+// a = 10
+// a = 10-4
+// a = 7
+// a = 7 + 2
+// a === 9
